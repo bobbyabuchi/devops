6c66bee156c03aeb72464540f5f2fa3e91d7e914
Bobby ABUCHI <bobbyabuchi@gmail.com>
2024-11-06 13:31:12 +0100

    Update placeholder.sh

--- placeholder.sh
+++ placeholder.sh
@@ -1,1 +1,29 @@
 
+version: '3'
+
+services:
+  web:
+    image: nginx:alpine
+    ports:
+      - "8080:80"
+    depends_on:
+      - app
+
+  app:
+    image: node:alpine
+    working_dir: /app
+    volumes:
+      - ./api:/app
+    command: sh -c "cd /app && npm install && node app.js"
+    depends_on:
+      - db
+
+  db:
+    image: mysql:5.7
+    environment:
+      MYSQL_ROOT_PASSWORD: root_password
+      MYSQL_DATABASE: sample_db
+      MYSQL_USER: app_user
+      MYSQL_PASSWORD: app_password
+    ports:
+      - "3306:3306"
